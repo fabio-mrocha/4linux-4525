130e41797ab7ba817d0a5f9a96eb9409df32037d
4Linux - Free Software Solutions <construtores@4linux.com.br>
2019-09-24 18:52:38 -0300

    Remoção de espaço em	include elastic_stack::nginx

--- arquivos/Aula 7.4/site.pp
+++ arquivos/Aula 7.4/site.pp
@@ -1,7 +1,7 @@
 node 'log' {
         include elastic_stack::java8
         include elastic_stack::repo
-	    include elastic_stack::nginx
+	include elastic_stack::nginx
         include elastic_stack::elasticsearch
         include elastic_stack::kibana
         include elastic_stack::logstash
